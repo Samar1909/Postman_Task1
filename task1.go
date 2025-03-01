c4c78bc418337f1d3c6cfdb4a86f506eaa59bef8
Simarjot <Simarjot1909@gmail.com>
2025-03-01 21:33:36 +0530

    Data Validation Done

--- task1.go
+++ task1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/xuri/excelize/v2"
 )
@@ -25,10 +26,41 @@ func main() {
 		fmt.Println(err)
 	}
 
-	selectedCols := make([]int, 6)
-	selectedCols.append(4, 5, 6, 7, 8, 9)
+	fmt.Println("Validating Data....")
+	for rowidx, row := range rows[1:] {
+		var sum float64 = 0
+		colidx := 4
+		for _, cellValue := range row[4:] {
+			if colidx == 8 {
+				colidx++
+				continue
+			}
+			if colidx == 10 {
+				break
+			}
+			val, err := strconv.ParseFloat(cellValue, 64)
+			if err != nil {
+				continue
+			}
+			sum += val
+			colidx++
+		}
 
-	for _, row := range rows {
+		cellstring := fmt.Sprintf("K%d", rowidx+2)
+		expectedSumStr, err := f.GetCellValue("CSF111_202425_01_GradeBook", cellstring)
 
+		if err != nil {
+			fmt.Println("error getting sum value for cell ", cellstring)
+		}
+		expectedSum, err := strconv.ParseFloat(expectedSumStr, 64)
+
+		if err != nil {
+			fmt.Println("error getting sum value for cell ", cellstring)
+		}
+		if expectedSum != sum {
+			fmt.Println("Wrong Total score for cell ", cellstring, ". The total score should be ", expectedSum)
+		}
 	}
+	fmt.Println()
+
 }
